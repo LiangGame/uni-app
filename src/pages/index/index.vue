98adef426d23c887b2056dd570f465644e562a41
Liang <15517072173@163.com>
2020-03-26 17:57:45 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="content">
+    <h1>111</h1>
+    <image class="logo" src="../../static/logo.png"></image>
+    <view>
+      <text class="title">{{title}}</text>
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data () {
+    return {
+      title: 'Hello',
+    };
+  },
+  onLoad () {
+    console.log('index onLoad');
+  },
+  methods: {},
+});
+</script>
+
+<style lang="scss">
+  @import "./index.scss";
+</style>
